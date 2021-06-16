66bf07d6ad6f128e0ffe00ea9c4babc2118de1ff
Altzen <msergsi@gmail.com>
2021-06-16 05:17:54 +0300

    First test after a long time)

---
+++ src/test/java/TestOne.java
@@ -0,0 +1,39 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import testRunnerTry.TestRunner;
+
+
+public class TestOne {
+    WebDriver driver = null;
+
+    @BeforeMethod
+    public void beforeMethod() {
+        TestRunner runner = new TestRunner();
+        driver = runner.runChrome();
+    }
+
+    @AfterMethod (alwaysRun = true)
+    public void afterMethod() {
+        driver.quit();
+    }
+
+    @Test
+    public void testTry() {
+        driver.get("https://www.leagueofgraphs.com/");
+        WebElement HOMESCREANSEARCHBUTTON = driver.findElement(By.xpath("//div[@id='filtersMenuSearchForm']//button[@class='search_button']"));
+        WebElement SEARCHFIELD = driver.findElement(By.xpath("//div[@id='filtersMenuSearchForm']//input[@name='wg_q']"));
+
+        Assert.assertEquals(HOMESCREANSEARCHBUTTON.getText(), "Search");
+        SEARCHFIELD.click();
+        SEARCHFIELD.sendKeys("Altzen");
+        driver.findElement(By.xpath("//div[@id='filtersMenuSearchForm']//button[@class='search_button']")).click();
+        WebElement PAGEBANNER = driver.findElement(By.xpath("//div[@class='pageBanner img-align-block']//h2"));
+        Assert.assertEquals(PAGEBANNER.getText(),"Search Results for \"Altzen\"");
+        driver.findElement(By.xpath("//div[@class='txt']/a[@href='/summoner/ru/Altzen']")).click();
+    }
+}
